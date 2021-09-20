a54054967190cf321c45c1a2a91205ae423c54db
Tomoya Kuroda <tomoya@impactnorth.com>
2021-09-19 23:02:35 -0400

    add angular-crash-2021

---
+++ angular-crash-2021/src/app/mock-tasks.ts
@@ -0,0 +1,15 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Learn Angular',
+    day: 'May 5th at 2:30 pm',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: 'Learn Angular',
+    day: 'May 5th at 2:30 pm',
+    reminder: true
+  },
+];
